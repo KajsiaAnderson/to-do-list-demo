40110e64a69845188764e368032f150afac03b2a
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-11-29 10:07:07 -0700

    to do list demo. passed in object props shortcut

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Item from './Item'
+
+const List = ({tasks}) => {
+
+    let taskDisplay = tasks.map((task) => {
+        return <Item task={task}/>
+    })
+
+    return (
+        <ul>
+            {taskDisplay}
+        </ul>
+    )
+}
+
+export default List
